8bfeddfecebd48b839f73110258f6def2586d72e
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-13 09:46:53 +0100

    another checker trial

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,4 +4,4 @@ monthly_savings = monthly_income - total_monthly_expenses
 simple_annual_interest = 0.05
 Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are: {monthly_savings:.2f}")
-print(f"Your projected savings after one year (with 5% interest): {Projected_savings:.2f}")
+print(f"Your projected savings after one year; with interest, is: {Projected_savings:.2f}")
