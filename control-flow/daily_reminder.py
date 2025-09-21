133de506a8094b27c659359baa37911aa3c10fed
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-21 17:06:39 +0100

    debugged for checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = input("Enter task description: ")
-time_bound = input("is this task time bound? (yes or no): ")
-priority = input("What is the task's priority (high, medium, low): ")
+task = input("Enter your task: ")
+time_bound = input("is it time bound? (yes or no): ")
+priority = input("priority (high, medium, low): ")
 match priority:
     case "high":
         if priority > time_bound:
