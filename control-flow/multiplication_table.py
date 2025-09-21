7b0f29d7b8c6aeef4ec3b930a0a4a2dda2aee8cd
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-21 15:38:19 +0100

    initial commit

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number to see its multiplication table: "))
+total = 0
+for i in range(1,12):
+    product = number * total
+    print(f"{number} * {total} = {product}")
+    total += 1 
