ea39f3f868726d3ea311b43dfb59ebb134f922ce
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-27 20:46:13 +0100

    second commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,16 +3,16 @@ def perform_operation(num1, num2, operation):
     num2 = float(input("Enter another number: "))
     operation = (input("Choose an operation: +, -, *, /"))
     match operation:
-        case "+":
+        case "add":
             addition = num1 + num2
             return addition
-        case "-":
+        case "subtract":
             subtraction = num1 - num2
             return subtraction
-        case "*":
+        case "multiply":
             multiplication = num1 * num2
             return multiplication
-        case "/":
+        case "divide":
             if num2 == 0:
                 print("Zero cannot be divided")
             else:
