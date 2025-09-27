dbbb017f922055fc423e6f4335eba147d034375a
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-27 20:40:48 +0100

    initial commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1, num2, operations):
+def perform_operation(num1, num2, operation):
     num1 = float(input("Enter a number: "))
     num2 = float(input("Enter another number: "))
     operation = (input("Choose an operation: +, -, *, /"))
