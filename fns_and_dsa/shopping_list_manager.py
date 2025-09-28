2df78a006ec93e0801b446bcad788b8adc03e926
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-28 14:15:46 +0100

    initial commit

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,27 @@
+def display_menu():
+    print("Shopping list manager")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
+    print("4. Exit")
+
+shopping_list[]
+while true:
+    display_menu()
+    choice = input("Enter your choice: ")
+    if choice == 1:
+        item_name = input("Enter item name: ")
+        shopping_list.append(item_name)
+        pass
+    elif choice == 2:
+        item_removed = input("Name of item removed: ")
+        shopping_list.append(item_removed)
+        pass
+    elif choice == 3:
+        shopping_list[]
+        pass
+    elif choice == 4:
+        print("Goodbye")
+        break
+    else:
+        print("Invalid choice. Try again")
