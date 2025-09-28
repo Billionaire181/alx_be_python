ec53a87704c9f23d7ed222fbc6591497b9d9581a
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-09-28 17:22:08 +0100

    initial commit

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+
+def display_current_datetime():
+    NOW = datetime.datetime.now()
+    current_date = NOW.strftime("%Y-%m-%D")
+    print(f"current date: {current_date}")
+
+def calculate_future_date(days):
+    today_date = datetime.now()
+    days = int(input("Enter the number of days to add to the current date: "))
+    time_gap = timedelta(days)
+    future_date = today_date + time_gap
+    formatted_future_date = future-date.strftime("%Y-%m-%D")
+    print(f"future date : {formatted_future_date}")
+
+display_current_datetime()
+calculate_future_date(days)
