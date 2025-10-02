bce9ad747b1ba6cb9ba5c171f310df849d2c9326
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-10-02 14:38:14 +0100

    initial commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        account_balance = 100
+
+    def deposit(self, amount):
+        result = amount + self.account_balance
+        return result
+
+    def withdraw(self, amount):
+        result = amount - self.account_balance
+        if amount < self.account_balance:
+            return True
+        else:
+            return False
+
+    def display_balance():
+        self.account_balance = result
+        print(f"Your balance is {self.account_balance}")
