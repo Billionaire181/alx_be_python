674b71f52d8aec3b46a0fa19d6ef985a169ecec5
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-10-04 23:39:35 +0100

    initial commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,22 +5,22 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def Test_addition(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
-        self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(1, 22), 23)
+        self.assertEqual(self.calc.addition(2, 3), 5)
+        self.assertEqual(self.calc.addition(-1, 1), 0)
+        self.assertEqual(self.calc.addition(1, 22), 23)
 
     def Test_subtraction(self):
-        self.assertEqual(self.calc.subtract(10, 5), 5)
-        self.assertEqual(self.calc.subtract(3, 2), 1)
-        self.assertEqual(self.calc.subtract(40, 30), 10)
+        self.assertEqual(self.calc.subtraction(10, 5), 5)
+        self.assertEqual(self.calc.subtraction(3, 2), 1)
+        self.assertEqual(self.calc.subtraction(40, 30), 10)
 
     def Test_multiplication(self):
-        self.assertEqual(self.calc.multiply(7, 2), 14)
-        self.assertEqual(self.calc.multiply(3, 5), 15)
-        self.assertEqual(self.calc.multiply(40, 3), 120)
+        self.assertEqual(self.calc.multiplication(7, 2), 14)
+        self.assertEqual(self.calc.multiplication(3, 5), 15)
+        self.assertEqual(self.calc.multiplication(40, 3), 120)
 
     def Test_division(self):
-        self.assertEqual(self.calc.divide(6, 2), 3)
-        self.assertEqual(self.calc.divide(20, 10), 2)
+        self.assertEqual(self.calc.division(6, 2), 3)
+        self.assertEqual(self.calc.division(20, 10), 2)
         with self.assertRaises(ZeroErrorDivision):
-            self.calc.divide(10, 0)
+            self.calc.division(10, 0)
