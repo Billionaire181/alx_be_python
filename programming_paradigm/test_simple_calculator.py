53171a5f5064efd2950963d6ddf7db16cd1e33b5
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-10-04 23:36:44 +0100

    initial commit

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,22 +4,22 @@ class TestSimpleCalculator(unittest.TestCase):
     def setup(self):
         self.calc = SimpleCalculator()
 
-    def Test_add(self):
+    def Test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(1, 22), 23)
 
-    def Test_subtract(self):
+    def Test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(3, 2), 1)
         self.assertEqual(self.calc.subtract(40, 30), 10)
 
-    def Test_multiply(self):
+    def Test_multiplication(self):
         self.assertEqual(self.calc.multiply(7, 2), 14)
         self.assertEqual(self.calc.multiply(3, 5), 15)
         self.assertEqual(self.calc.multiply(40, 3), 120)
 
-    def Test_divide(self):
+    def Test_division(self):
         self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertEqual(self.calc.divide(20, 10), 2)
         with self.assertRaises(ZeroErrorDivision):
