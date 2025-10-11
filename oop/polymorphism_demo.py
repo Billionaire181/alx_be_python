520dfcf892f7d8de1e121e2ef4c0d5b26f2d42cd
Victor_Billionaire <victorkamsiyochukwu100@gmail.com>
2025-10-11 12:32:37 +0100

    initial

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,9 +4,16 @@ class Shape:
         raise NotimplementedError
 
 class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
     def area(self, legth, width):
-        return length * width
+        return self.length * self.width
 
 class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
     def area(self, radius):
-        return math.pi * radius **2
+        return math.pi * self.radius ** 2
